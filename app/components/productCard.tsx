ca7f3d55875355466061b5d20390b6e3699228d1
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2024-12-19 01:29:37 +0500

    images fixed

--- app/components/productCard.tsx
+++ app/components/productCard.tsx
@@ -5,7 +5,7 @@ const Card = (product: { image: string, name: string, amount: string }) => {
     return (
         <div className='flex flex-col w-[287px] h-[372px] justify-center '>
             <div className='w-full h-full flex justify-center items-center'>
-                <Image src={`/${product.image}`} width={287} height={287} alt='image' />
+                <Image src={`/products/${product.image}`} width={287} height={287} alt='image' />
             </div>
 
             <h1>{product.name}</h1>
